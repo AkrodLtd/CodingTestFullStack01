94c1fa0acae98dc9ff9b12f9f48d8d105beec785
Rodrigo Feijao <hello@rodrigofeijao.com>
2021-03-31 23:32:49 +0100

    nodeversion updated for lambdas

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -37,7 +37,7 @@ resource "aws_lambda_function" "user_service_lambda" {
   role             = aws_iam_role.iam_for_lambda_tf.arn
   handler          = "index.handler"
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
-  runtime          = "nodejs12.x"
+  runtime          = "nodejs14.x"
 }
 
 # API GATEWAY
