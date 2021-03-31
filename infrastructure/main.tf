ce1dbadd97df9c24af350ca54aa9fe2d8f5d9c2c
Rodrigo Feijao <hello@rodrigofeijao.com>
2021-03-31 23:30:55 +0100

    Add initial terraform

---
+++ infrastructure/main.tf
@@ -0,0 +1,96 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
+# Lambda Source
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_file = "index.js"
+  output_path = "lambda_function.zip"
+}
+
+# IAM
+resource "aws_iam_role" "iam_for_lambda_tf" {
+  name = "iam_for_lambda_tf"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+# Lambda
+resource "aws_lambda_function" "user_service_lambda" {
+  filename         = "lambda_function.zip"
+  function_name    = "user_service_lambda"
+  role             = aws_iam_role.iam_for_lambda_tf.arn
+  handler          = "index.handler"
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+  runtime          = "nodejs12.x"
+}
+
+# API GATEWAY
+resource "aws_api_gateway_rest_api" "user_service_api_gateway" {
+  name        = "UserServiceGateway"
+  description = "Terraform Serverless Application"
+}
+
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.user_service_api_gateway.id
+  parent_id   = aws_api_gateway_rest_api.user_service_api_gateway.root_resource_id
+  path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id   = aws_api_gateway_rest_api.user_service_api_gateway.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda" {
+  rest_api_id = aws_api_gateway_rest_api.user_service_api_gateway.id
+  resource_id = aws_api_gateway_method.proxy.resource_id
+  http_method = aws_api_gateway_method.proxy.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.user_service_lambda.invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "user_service_api_gateway" {
+  depends_on = [
+    aws_api_gateway_integration.lambda,
+  ]
+
+  rest_api_id = aws_api_gateway_rest_api.user_service_api_gateway.id
+  stage_name  = "api"
+}
+
+# Lambda Permissions
+resource "aws_lambda_permission" "user_service_api_gateway" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.user_service_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  # The "/*/*" portion grants access from any method on any resource
+  # within the API Gateway REST API.
+  source_arn = "${aws_api_gateway_rest_api.user_service_api_gateway.execution_arn}/*/*"
+}
+
+# OUTPUT
+output "base_url" {
+  value = aws_api_gateway_deployment.user_service_api_gateway.invoke_url
+}
